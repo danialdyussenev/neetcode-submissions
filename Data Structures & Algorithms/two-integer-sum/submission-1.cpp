119eee646b5a7bbc0c8abbbebe9b326816e630e7
danialdyussenev <86018855+danialdyussenev@users.noreply.github.com>
2026-09-13 14:39:45 +0500

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(std::size_t i{};i<nums.size();++i){
+            for(std::size_t j{i+1};j<nums.size();++j){
+                if(nums[i]+nums[j] == target){
+                    return {static_cast<int>(i),static_cast<int>(j)};
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+
+        
+    }
+};
